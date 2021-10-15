3ab606084d0e0f990df89462319190169b7ea581
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-10-15 23:08:01 +0530

    c library

---
+++ cmake-build-debug/CMakeFiles/c_library.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_library.dir/library.c.obj"
+  "c_library.lib"
+  "c_library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/c_library.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "c_library.lib"
+)
